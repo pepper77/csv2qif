3156ce134f9d2baa5a2d9ec275ab0cc6e0e179da
Pål Arne Hoff <github@hoff.ol.no>
2011-08-11 18:30:55 +0200

    Initial GUI-design.

---
+++ src/MainGUI.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+/**
+ * Created by Pål Arne Hoff.
+ * Date: 11.08.11
+ * Time: 16:07
+ */
+public class MainGUI {
+    private JButton saveToQIFButton;
+    private JTabbedPane tabbedPane1;
+    private JComboBox comboBox1;
+    private JTable table1;
+    private JToolBar toolBar;
+}
